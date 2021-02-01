6520987871ab6f4c5225736172980da8d2f091bc
MikeShahin <gobias101@gmail.com>
2021-02-01 15:55:19 -0800

    submit
    submit

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  let time = string.split(":");
+  let hour = parseInt(time[0]);
+
+  if (hour < 12) return "Good Morning"
+  if (hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
+
 /* Write your implementation of displayMessage() */
+let h1 = document.getElementById("greeting");
+
+function displayMessage(greeting) {
+    h1.innerText = greeting
+}
